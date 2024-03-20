34ff87f3b1a4e46773924be3842b37c0edcf5080
Gustavo Pereyra <gustavodpereyra@gmail.com>
2024-03-20 09:00:38 -0300

    añadido imprimir

---
+++ Imprimir.php
@@ -0,0 +1,12 @@
+<html>
+    <head></head>
+    <body>
+        <h1> Mi segundo archivo en php</h1>
+        <?php
+        echo <<<POLLO
+        en un mundo conectado 
+        donde cada segundo se envia millones de correos electronicos...
+POLLO;
+?>
+    </body>
+    </html>
