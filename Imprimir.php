8df123699324cb21c1840c63fe3be5dc9cc805a6
Gustavo Pereyra <gustavodpereyra@gmail.com>
2024-03-20 09:10:55 -0300

    añadido comentarios a imprimir

--- Imprimir.php
+++ Imprimir.php
@@ -3,10 +3,12 @@
     <body>
         <h1> Mi segundo archivo en php</h1>
         <?php
-        echo <<<POLLO
+        echo <<<POLLO //le puse pollo porque se me hizo gracioso
         en un mundo conectado 
         donde cada segundo se envia millones de correos electronicos...
 POLLO;
-?>
+/* no olvidar cerrar el php
+con ?> */
+?> 
     </body>
     </html>
