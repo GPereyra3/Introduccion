ec8889de40f452d90daff4c24258d2338dd9e7b1
Gustavo Pereyra <gustavodpereyra@gmail.com>
2024-04-10 09:14:49 -0300

    añadido archivos

---
+++ constantes.php
@@ -0,0 +1,9 @@
+<html>
+    <head></head>
+    <body>
+        <?php
+        define ('Pi', 3.1416)
+        define ('Pi', 3.14);
+?> 
+    </body>
+    </html>
---
+++ variables.php
@@ -0,0 +1,13 @@
+<html>
+    <head></head>
+    <body>
+        <?php
+        $texto = "texto";
+        $numerica = 10;
+        $float = 5.5;
+        var_dump($texto);
+        var_dump($numerica);
+        var_dump($float);
+?> 
+    </body>
+    </html>
